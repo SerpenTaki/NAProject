b9fe8eefeed635e0bfef23a8336180aed1b9fe4d
MicheleDioli <micheledioli.dm@gmail.com>
2025-02-05 17:44:43 +0100

    update multigeo

--- multigeo.m
+++ multigeo.m
@@ -1,26 +1,21 @@
-function [k] = multigeo(A, l, toll)
-    %PRE: A matrice quadrata reale o complessa, l scalare complesso, toll
-    %scalare reale positivo
-    if size(A,1) ~= size(A,2)
-        error("Matrice non quadrata");
-    end
-    if det(A) == 0 
-        error("Matrice non invertibile");
+function [k] = multigeo(A, toll)
+    % PRE: A is a square real or complex matrix, toll is a positive real scalar
+    % POST: k is the rank of the matrix A
+
+    if size(A, 1) ~= size(A, 2)
+        error("Matrix must be square");
     end
 
-    [L,U,P] = lu(A);
-    rank = 1;
+    
+    [L, U, P] = lu(A);
 
-    for i = 1:size(U,1)
-        if abs(U(i,i)) < toll
-            break;
+    rank = 0;
+
+    for i = 1:size(U, 1)
+        if abs(U(i, i)) > toll
+            rank = rank + 1;
         end
-        if U(i,i) == 0
-            rank = rank+1;
-        end    
     end
 
     k = rank;
-    
 end
-    %POST: la funzione restituisce k reale positivo dimensione dell'autospazio
