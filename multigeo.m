2d839d14a53954157c13174a5113a10a1cd2f925
SerpenTaki <gabrielecorubolo@gmail.com>
2025-02-05 17:37:44 +0100

    update

--- multigeo.m
+++ multigeo.m
@@ -1,4 +1,26 @@
 function [k] = multigeo(A, l, toll)
-    
+    %PRE: A matrice quadrata reale o complessa, l scalare complesso, toll
+    %scalare reale positivo
+    if size(A,1) ~= size(A,2)
+        error("Matrice non quadrata");
+    end
+    if det(A) == 0 
+        error("Matrice non invertibile");
+    end
 
+    [L,U,P] = lu(A);
+    rank = 1;
+
+    for i = 1:size(U,1)
+        if abs(U(i,i)) < toll
+            break;
+        end
+        if U(i,i) == 0
+            rank = rank+1;
+        end    
+    end
+
+    k = rank;
+    
 end
+    %POST: la funzione restituisce k reale positivo dimensione dell'autospazio
