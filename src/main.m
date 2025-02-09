68e033677079a15ea806acda07976341b4a63d4f
MicheleDioli <micheledioli.dm@gmail.com>
2025-02-09 17:59:52 +0100

    primi test

--- src/main.m
+++ src/main.m
@@ -3,20 +3,27 @@ clear all;
 close all;
 
 % Dimensione della matrice
-n = 5;
+n = 3;
     
 % Creazione di una matrice test con autovalori noti
 lambda = [2, 2, 3, 3, 3];
 J = blkdiag([2 1; 0 2], [3 1 0; 0 3 1; 0 0 3]);
 Q = orth(randn(n)); %genera una matrice ortogonale Q di dim nxn
-A = Q' * J * Q;
+%A = Q' * J * Q;
     
 % Autovalore target
-lO = 2;
+lO = 3;
+
+%test manuale
+
+A = [5,4,2; 0,3,-1; 0,0,3]; %Diagonalizzabile auto= 5, 3 
+%A = [4,1,0; 0,4,1; 0,0,4]; % Non diagonalizzabile auto 4
+
+% geo <= alg -> diagonlizzabile 
     
 % Parametri
 toll = 1e-6;
-it = 5;
+it = 4;
 maxit = 50;
     
 % Test multgeo
@@ -30,7 +37,7 @@ fprintf('f(%f) = %f, g(%f) = %f\n', lO, f, lO, g);
 % Test multalg
 [l, m, flag] = multialg(A, lO, toll, it, maxit);
 if flag
-    fprintf('Autovalore calcolato: %f con molteplicità %d\n', l, m);
+    fprintf('Autovalore calcolato: %f con molteplicità A %d\n', l, m);
 else
     fprintf('Errore nel calcolo dell''autovalore.\n');
 end
