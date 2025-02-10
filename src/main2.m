9854928d052755d188a82797b593f8bad0f663ce
SerpenTaki <gabrielecorubolo@gmail.com>
2025-02-10 15:04:47 +0100

    Update main2.m

--- src/main2.m
+++ src/main2.m
@@ -3,7 +3,7 @@ close all;
 clear all;
 
 % Inizializzo matrici sia a livello di dimensione che per contenuto
-A = eye(2) * 2;
+A = eye(4) * 4;
 B = eye(3) * 3;
 dimA = size(A,1);
 dimB = size(B,1);
@@ -33,3 +33,27 @@ for i = 2:n
 end
 
 disp(C);
+
+
+
+% Parametri
+toll = 1e-6;
+it = 4;
+maxit = 50;
+lO = 4;    
+
+% Test multgeo
+k = multigeo(A, lO, toll);
+fprintf('Molteplicità geometrica di %f: %d\n', lO, k);
+    
+% Test myobjective
+[f, g] = myobjective(lO, A);
+fprintf('f(%f) = %f, g(%f) = %f\n', lO, f, lO, g);
+    
+% Test multalg
+[l, m, flag] = multialg(A, lO, toll, it, maxit);
+if flag
+    fprintf('Autovalore calcolato: %f con molteplicità A %d\n', l, m);
+else
+    fprintf('Errore nel calcolo dell''autovalore.\n');
+end
